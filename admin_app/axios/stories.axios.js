2c41c581acc951806d8129780b7997f75008ac3b
Lohith <srklohith05@gmail.com>
2022-04-12 17:32:28 +0530

    Changes

--- admin_app/axios/stories.axios.js
+++ admin_app/axios/stories.axios.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const baseUrl = 'https://able-server.herokuapp.com/';
+// const baseUrl = 'https://able-server.herokuapp.com/';
+const baseUrl = 'http://192.168.0.106:8080';
 
 export const getStories = () => axios.get(`${baseUrl}admin/stories`);
 
 export const getParticularStory = (id) =>
 	axios.get(`${baseUrl}admin/stories/${id}`);
 
-export const updateOrInsertStories = (values, id, photo) =>
-	axios.post(
+export const updateOrInsertStories = (values, id, photo) => {
+	return axios.post(
 		id ? `${baseUrl}admin/stories/${id}` : `${baseUrl}admin/stories/`,
 		{ values, photo }
 	);
-
+};
 export const deleteStories = (id, values) =>
 	axios.delete(`${baseUrl}admin/stories/${id}`, values);
